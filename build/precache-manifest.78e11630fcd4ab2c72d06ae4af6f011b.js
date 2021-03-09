336037df0863034f4c964d74d6ce602bb2342c32
Usman Shery <usman.shery@gmail.com>
2021-03-09 21:27:38 +0500

    moving the domain, try 1

---
+++ build/precache-manifest.78e11630fcd4ab2c72d06ae4af6f011b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7895deebb556bbd00883a8e3504fa9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8d38d79a3e6bb48ddd7",
+    "url": "./static/css/2.3596d239.chunk.css"
+  },
+  {
+    "revision": "3f5556f5e0b9a56f12f3",
+    "url": "./static/css/main.9a4b07ae.chunk.css"
+  },
+  {
+    "revision": "f8d38d79a3e6bb48ddd7",
+    "url": "./static/js/2.20638c37.chunk.js"
+  },
+  {
+    "revision": "95549b853158ee9519e736efe5cf4e35",
+    "url": "./static/js/2.20638c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5556f5e0b9a56f12f3",
+    "url": "./static/js/main.b56d3c81.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b82603367c06a9979f0601e1ad7eb108.js"
+  "./precache-manifest.78e11630fcd4ab2c72d06ae4af6f011b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
